59b1986ea9dd64854262b498297839ab9b4e7d3b
Felipe Augusto <felipeaugustodoss@yahoo.com.br>
2015-03-24 17:23:38 -0300

    Exercicio sem ArrayList

---
+++ Exercicios/SemArrayList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int k, n, r;
+            n = 1;
+            k = -1;
+            r = 0;
+
+            while (n != 0)
+            {
+                Console.Write("N: ");
+                n = int.Parse(Console.ReadLine());
+                r = n + r;
+                k++;
+            }
+
+            r = r / k; 
+
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
