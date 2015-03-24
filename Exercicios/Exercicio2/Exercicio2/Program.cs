ffdd45406c3a2a71a54d9c7fbe919869fe7cbda2
Felipe Augusto <felipeaugustodoss@yahoo.com.br>
2015-03-24 17:36:41 -0300

    Com ArrayList

---
+++ Exercicios/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int r = 0;
+            ArrayList ns = new ArrayList();
+
+            while (n != 0)
+            {
+                n = int.Parse(Console.ReadLine());
+
+                r = r + n;
+
+                if (n > 0)
+                {
+                    ns.Add(n);
+                }
+
+            }
+
+            r = r / ns.Count;
+
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
