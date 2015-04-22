337d39ddefe73f6e1b553211154c41467d8cc0db
Felipe Augusto <felipeaugustodoss@yahoo.com.br>
2015-04-21 23:16:03 -0300

    Exercício potenciação - 31/03/2015

---
+++ Exercicios/Aula 31_03_2015/Aula 31_03_2015/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_31_03_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exp, x;
+            int r = 1;
+
+            Console.Write("Número: ");
+            exp = int.Parse(Console.ReadLine());
+            Console.Write("Expoente: ");
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                r *= exp;
+            }
+
+            Console.WriteLine("O resultado é {0}.", r);
+
+            Console.ReadKey();
+        }
+    }
+}
