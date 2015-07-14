cafa6b916f5aaa59014368e22dffbc20ea2cc63c
Felipe Augusto dos Santos <felipeaugustodoss@yahoo.com.br>
2015-07-14 17:38:06 -0300

    Programa do Cachorro Fido - 14/07/2015

---
+++ Exercicios/2 Certificação/Exercicio-14-07-2015/Exercicio-14-07-2015/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_14_07_2015
+{
+    class Cachorro
+    {
+      public  int _tamanhoCm;
+      public string _nome;
+      public double _peso;
+  
+
+      public void SetNome(string nome)
+    {
+        _nome = nome;   
+    }
+
+        public string GetNome()
+      {
+          return _nome;
+      }
+      public void SetPeso(double peso)
+      {
+          _peso = peso;
+
+      }
+        public double GetPeso()
+      {
+          return _peso;
+      }
+
+        public double RacaoDiariaKg()
+        {
+            return (_peso / ((_tamanhoCm / 100) * (_tamanhoCm / 100))) / 10;
+        }
+    }
+}
---
+++ Exercicios/2 Certificação/Exercicio-14-07-2015/Exercicio-14-07-2015/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_14_07_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro fido = new Cachorro();
+            fido._tamanhoCm = 95;
+            fido.SetNome("Fido");
+            fido.SetPeso(42.3);
+
+           Console.WriteLine("Nome: {0}",fido.GetNome());
+            Console.WriteLine("Peso: {0}",fido.GetPeso());
+            Console.WriteLine("Tamanho (m): {0}",fido._tamanhoCm);
+
+            Console.WriteLine("Quantidade diária de ração: {0}", fido.RacaoDiariaKg());
+
+            Console.ReadKey();
+
+
+            // ----------------> ERRO <<<<<<<<<<<<<<<<<<<<<---------- \\
+
+            // O ERRO ESTÁ NO _tamanhoCm não possuir Get e Set, isto pode gerar grandes futuros problemas.
+           
+        }
+    }
+}
