c66a1b6759715637438b3134130c34c5341e962d
Felipe Augusto <felipeaugustodoss@yahoo.com.br>
2015-08-25 17:32:23 -0300

    Aula recuperação - 25/08

--- Aulas/Aula Rec 2 Cert - 25-08/Aula Rec 2 Cert - 25-08/Atleta.cs
+++ Aulas/Aula Rec 2 Cert - 25-08/Aula Rec 2 Cert - 25-08/Atleta.cs
@@ -8,5 +8,16 @@ namespace Aula_Rec_2_Cert___25_08
 {
     class Atleta
     {
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+        public int TempoCs { get; set; }
+
+        public string TempoFormat
+        {
+            get
+            {
+                return String.Format("{0}:{1}:{2}", TempoCs / 6000, (TempoCs % 6000) / 100, (TempoCs % 6000) % 100);
+            }
+        }
     }
 }
