24705862b076e90bb9a2b07e25274a3423184017
Felipe Augusto <felipeaugustodoss@yahoo.com.br>
2015-09-15 17:09:00 -0300

    Aula 15/09/2015 - Com herança

---
+++ Aulas/3Certificacao/COMHERANCA/ConsoleApplication1/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public int CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double Salario { get; set; }
+        public string Matricula { get; set; }   
+    }
+}
---
+++ Aulas/3Certificacao/COMHERANCA/ConsoleApplication1/Tercerizados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Tercerizados
+    {
+        public string Nome { get; set; }
+        public int CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Matricula { get; set; }
+    }
+}
